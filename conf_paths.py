eddb209862956ac248ec860cf1b51c977f7c5092
Victor Vasilev <victor.dimitrov.vasilev@gmail.com>
2023-11-12 20:22:32 +0200

    Add files via upload

---
+++ conf_paths.py
@@ -0,0 +1,25 @@
+gdp_prod_path = "datasets/GDP_production_nsi.csv"
+gdp_path = "datasets/bulgaria-gdp-macrotrends.csv"
+gdp_growth_path = "datasets/bulgaria-gdp-growth-rate-macrotrends.csv"
+gdp_per_capita_path = "datasets/bulgaria-gdp-per-capita-macrotrends.csv"
+debt_to_gdp_path = "datasets/bulgaria-debt-to-gdp-ratio-macrotrends.csv"
+gni_path = "datasets/bulgaria-gni-macrotrends.csv"
+gni_per_capita_path = "datasets/bulgaria-gni-per-capita-macrotrends.csv"
+
+inflation_rate_path = "datasets/bulgaria-inflation-rate-cpi-macrotrends.csv"
+economic_growth_path = "datasets/bulgaria-economic-growth-rate-macrotrends.csv"
+
+immigration_path = "datasets/bulgaria-immigration-statistics-macrotrends.csv"
+net_migration_path = "datasets/bulgaria-net-migration-macrotrends.csv"
+
+births_by_mother_age_path = "datasets/births_by_age_of_mother_total_nsi.csv"
+deaths_by_age_path = "datasets/deaths_by_age_nsi.csv"
+deaths_by_sex_path = "datasets/deaths_by_sex_nsi.csv"
+migration_path = "datasets/external_migration_bg_nsi.csv"
+
+population_path = 'datasets/bulgaria-population-macrotrends.csv'
+life_expectancy_path = 'datasets/bulgaria-life-expectancy-macrotrends.csv'
+birth_rate_path = 'datasets/bulgaria-birth-rate-macrotrends.csv'
+death_rate_path = 'datasets/bulgaria-death-rate-macrotrends.csv'
+fertility_rate_path = 'datasets/bulgaria-fertility-rates.csv'
+unemployment_rate_path = 'datasets/bulgaria-unemployment-rate-macrotrends.csv'
